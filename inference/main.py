525862d1169025a97025064b4315c8bcaa8b7375
aghyad-deeb <th3elctronicag@gmail.com>
2026-01-25 14:38:59 -0800

    working inference and adapter loading/unloading

---
+++ inference/main.py
@@ -0,0 +1,117 @@
+import aiohttp
+import requests
+from sglang.srt.entrypoints.http_server import launch_server
+from sglang.srt.server_args import ServerArgs
+
+async def async_get(url):
+    session = aiohttp.ClientSession()
+    resp = await session.request("GET", url)
+    await session.close()
+    return resp
+
+async def async_post(url, json):
+    session = aiohttp.ClientSession()
+    resp = await session.request("POST", url, json=json)
+    await session.close()
+    return resp
+
+class InferenceServer:
+
+    MAX_RUNNING_REQUESTS = 256
+    HOST = "127.0.0.1"
+    PORT = 8112
+    BASE_URL = f"http://{HOST}:{PORT}"
+    FIXED_LORA_NAME = "only_lora"
+
+    def __init__(self, rollout_args):
+        self.rollout_args = rollout_args
+        # We override only parts specified in rollout config to keep model 
+        # defaults if not specified (e.g. best temperature varies by model)
+        self.default_sampling_params = rollout_args.default_sampling_params
+        # By having this None, we default to using no-loras
+        self.curr_lora_name = None
+
+    async def check_health(self):
+        url = f"{self.BASE_URL}/health"
+        response = await async_get(url)
+        # print(response.json)
+        return response.status == 200
+
+    def start_server(self):
+        server_args = ServerArgs(
+            model_path=self.rollout_args.model_id,
+            host=self.HOST,
+            port=self.PORT,
+            dp_size=self.rollout_args.data_parallel_size,
+            tp_size=self.rollout_args.tensor_parallel_size,
+            mem_fraction_static=self.rollout_args.mem_fraction_static,
+            trust_remote_code=True,
+            enable_lora=True,
+            max_lora_rank=self.rollout_args.lora_rank,
+            lora_target_modules=self.rollout_args.lora_target_modules,
+            max_running_requests=self.MAX_RUNNING_REQUESTS,
+            load_balancing_method="auto",
+        )
+        launch_server(server_args)
+
+    # dp_size must be 1 for lora weight syncing
+    async def load_lora_adapter(self, lora_path):
+        assert await self.check_health()
+        if self.curr_lora_name != None:
+            unload_url = f"{self.BASE_URL}/unload_lora_adapter"
+            unload_response = await async_post(
+                unload_url,
+                json=dict(
+                    lora_name=self.FIXED_LORA_NAME,
+                )
+            )
+            assert unload_response.status == 200, f"{unload_response.status=}\n{await unload_response.json()=}"
+
+        load_url = f"{self.BASE_URL}/load_lora_adapter"
+        load_response = await async_post(
+            load_url,
+            json=dict(
+                lora_name=self.FIXED_LORA_NAME,
+                lora_path=lora_path,
+                pinned=True, # avoids unloading and reloading adapter
+            )
+        )
+        assert load_response.status == 200, f"{load_response.status=}\n{await load_response.json()=}"
+        self.curr_lora_name = self.FIXED_LORA_NAME
+        return load_response
+
+    async def generate(self, input_tokens, sampling_params=None):
+        assert await self.check_health()
+        if not sampling_params:
+            sampling_params = self.default_sampling_params
+        generate_json = dict(
+            input_ids=input_tokens,
+            sampling_params=sampling_params,
+            lora_path=self.curr_lora_name,
+        )
+        url = f"{self.BASE_URL}/generate"
+        response = await async_post(
+            url,
+            json=generate_json
+        )
+        return response
+
+
+# For development
+if __name__ == "__main__":
+    server_args = ServerArgs(
+        model_path="Qwen/Qwen3-0.6B",
+        host=InferenceServer.HOST,
+        port=InferenceServer.PORT,
+        dp_size=1,
+        tp_size=1,
+        enable_lora=True,
+        max_lora_rank=32,
+        lora_target_modules=["q_proj", "k_proj", "v_proj",  "o_proj", "gate_proj", "up_proj", "down_proj"],
+        mem_fraction_static=0.85,
+        trust_remote_code=True,
+        max_running_requests=256,
+        load_balance_method="auto",
+        reasoning_parser="qwen3"
+    )
+    launch_server(server_args)
--- scratch.py
+++ scratch.py
@@ -1,1 +1,75 @@
+# %%
 from rollout.launch import check_health
+
+# %%
+resp = check_health()
+# %%
+resp.status_code == 200
+# %%
+from rollout.launch import load_lora_adapter
+
+# %%
+resp = load_lora_adapter("blah")
+# %%
+resp.json()
+# %%
+resp.status_code
+
+# %%
+import aiohttp 
+
+async def async_get(url):
+    session = aiohttp.ClientSession()
+    resp = await session.request("GET", url)
+    await session.close()
+    return resp
+
+resp = await async_get("http://localhost:8112/health")
+# %%
+resp.content
+
+# %%
+async def async_post(url, json):
+    session = aiohttp.ClientSession()
+    resp = await session.request("POST", url, json=json)
+    await session.close()
+    return resp
+
+
+# %%
+from transformers import AutoTokenizer
+t = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6b")
+messages = [
+    {
+        "role":"user", 
+        "content": "ما عاصمة الصين؟"
+    }
+]
+ids = t.apply_chat_template(messages, add_generation_prompt=True)
+sampling_params = dict(max_new_tokens=3000)
+# ids = t(text)
+resp = await async_post("http://localhost:8112/generate", json=dict(input_ids=[ids, ids], sampling_params=sampling_params, lora_path=[None, infs.FIXED_LORA_NAME]))
+# await resp.json()
+# %%
+await resp.json()
+
+# %%
+await resp.json()
+# %%
+t.decode(ids, skip_special_tokens=False)
+# %%
+from inference.launch import InferenceServer
+from dataclasses import dataclass, field
+
+@dataclass
+class RolloutArgs:
+    blah: int = 0
+    default_sampling_params: dict = field(default_factory=dict)
+
+rg = RolloutArgs()
+infs = InferenceServer(rg)
+# %%
+
+resp = await infs.load_lora_adapter("YoussefHosni/Qwen3-0.6b-2-Token-arabic-LoRA-finetuned")
+await resp.json()
+# %%
