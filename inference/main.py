90455600c163758da2975739a7290d2ace864b36
aghyad-deeb <th3elctronicag@gmail.com>
2026-02-28 15:51:13 -0800

    cleaned up infer dev stuff

--- inference/main.py
+++ inference/main.py
@@ -95,23 +95,3 @@ class InferenceServer:
             json=generate_json
         )
         return response
-
-
-# For development
-if __name__ == "__main__":
-    server_args = ServerArgs(
-        model_path="Qwen/Qwen3-0.6B",
-        host=InferenceServer.HOST,
-        port=InferenceServer.PORT,
-        dp_size=1,
-        tp_size=1,
-        enable_lora=True,
-        max_lora_rank=32,
-        lora_target_modules=["q_proj", "k_proj", "v_proj",  "o_proj", "gate_proj", "up_proj", "down_proj"],
-        mem_fraction_static=0.85,
-        trust_remote_code=True,
-        max_running_requests=256,
-        load_balance_method="auto",
-        reasoning_parser="qwen3"
-    )
-    launch_server(server_args)
