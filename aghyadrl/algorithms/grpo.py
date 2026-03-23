added proper masking and now calculating correct on-policy grpo objective (though it's async so not on-policy, so needs to adjust for logprobs)

aghyadrl/algorithms/grpo.py
@@ -2,17 +2,18 @@ import asyncio
 import torch
 from jaxtyping import Int, Float
 from beartype import beartype
-from aghyadrl.inference import InferenceServer, InferenceArgs
 from transformers import AutoTokenizer
 import multiprocessing as mp
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
 from typing import Optional
 import pandas as pd
+
+from aghyadrl.inference import InferenceServer, InferenceArgs
+from aghyadrl.train import Trainer, TrainingArgs
 
 @dataclass
 class GRPOArgs:
     mini_bsz: int = 16
-    micro_bsz: int = 4
     group_size: int = 8
 
     max_prompt_len: int = 200
@@ -32,6 +33,63 @@ class Datum:
     advantage: Optional[float] = None
 
 
+@dataclass
+class MicroBatch:
+    trajs_lst: list[list[dict]] = field(default_factory=list)
+    prompts: list[list[int]] = field(default_factory=list)
+
+    max_prompt_len: int = -1
+    max_response_len: int = -1
+
+def decode_tokens_individually(tensor):
+    t = AutoTokenizer.from_pretrained("Qwen/Qwen3-8b")
+    return t.batch_decode(tensor)
+
+@beartype
+def reinforce_loss(
+    inputs: Int[torch.Tensor, "bsz max_seq_len"],
+    logprobs: Float[torch.Tensor, "bsz max_seq_len vocab_size"],
+    # we only need old_logprobs (and logprobs) for chosen tokens
+    old_logprobs: Float[torch.Tensor, "bsz max_seq_len"], #~ assuming on-policy for now so not used
+    #! Wait they still have the length normalization in the GRPO paper
+    advantages: Float[torch.Tensor, "bsz"],
+    response_masks: Int[torch.Tensor, "bsz max_seq_len"]
+) -> Float[torch.Tensor, ""]:
+    # print(f"{logprobs.tolist()=}")
+    response_lengths: Int[torch.Tensor, "bsz"] = response_masks.sum(-1)
+    #TODO I think I should sum after multiplying by response mask, but why is the
+    #TODO logprobs for pad changing? is that normal?
+    #! I'm suspicious that padding isn't supposed to use the pad_token_id, 
+    #! the model's highest logprobs are for im_start.
+    # It seems like this is fine, we'll multiple by response mask anyway
+
+    #TODO there's also a hang I should investigate. edit: I think there isn't
+    #! for debugging
+    sampled_tokens = logprobs.argmax(-1)
+    output_tokens_logprobs = torch.gather(logprobs, -1, inputs.unsqueeze(-1)).squeeze(-1)
+
+    # We remove the last token as it was not sampled in inference
+    output_tokens_logprobs = output_tokens_logprobs[:,:-1]
+    # We shift the mask by one to match the output tokens
+    output_tokens_mask = response_masks[:, 1:]
+    # for i in range(inputs.shape[0]):
+    #     print(f"{[(a, b, c, d) for a, b, c, d in zip(decode_tokens_individually(inputs[i]), response_masks[i], decode_tokens_individually(sampled_tokens[i]), output_tokens_mask[i])]=}")
+        # seems to be working! Mask output when it should
+    # We mask the output tokens that don't belong to the trajectory (prompt 
+    # tokens + pad tokens output + last token not used for trajectory)
+    output_tokens_logprobs *= output_tokens_mask
+    traj_logprob: Float[torch.Tensor, "bsz"] = (
+        output_tokens_logprobs.sum(-1)
+    ) / response_lengths
+    # I wrote out the math; after differentiation, this should give the sample
+    # mean of the gradient of the grpo objective
+    losses: Float[torch.Tensor, "bsz"] = traj_logprob * advantages
+    # to turn from objective to loss
+    losses *= -1
+    mean = losses.mean()
+    print(f"{mean=}")
+    return mean
+
 async def generate_group(
     inference_server: InferenceServer,
     prompt_ids: list[int],
@@ -42,8 +100,13 @@ async def generate_group(
         for _ in range(group_size)
     ]
     returns =  await asyncio.gather(*tasks)
-    jsons = [await ret.json() for ret in returns]
-    return (jsons, prompt_ids)
+    max_response_len = 0
+    trajs = []
+    for ret in returns:
+        traj = await ret.json()
+        max_response_len = max(max_response_len, len(traj["output_ids"]))
+        trajs.append(traj)
+    return (trajs, prompt_ids, max_response_len)
 
 async def generate_batch(
     inference_server: InferenceServer,
@@ -58,17 +121,26 @@ async def generate_batch(
         for prompt_tensor in batch
     ]
 
-    to_return = []
     pending = tasks    
     while pending:
+        # We grab the completed groups as soon as they're done since we don't 
+        # need them to be in order, but keep each group together for advantage 
+        # calculation
+        # We store the current prompt_ids len and response_len so we can pad 
+        # accordingly and forward_backward
+        mb = MicroBatch()
         done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
         for done_task in done:
-            to_return.append(done_task.result())
-            if len(to_return) == micro_bsz:
-                yield to_return
-                to_return = []
-    if len(to_return) > 0:
-        yield to_return
+            trajs, prompt_ids, group_max_response_len = done_task.result()
+            mb.trajs_lst.append(trajs)
+            mb.prompts.append(prompt_ids)
+            mb.max_prompt_len = max(mb.max_prompt_len, len(prompt_ids))
+            mb.max_response_len = max(mb.max_response_len, group_max_response_len)
+            if len(mb.trajs_lst) == micro_bsz:
+                yield mb
+                mb = MicroBatch()
+    if len(mb.trajs_lst) > 0:
+        yield mb.trajs_lst
 
 def compute_reward(text):
     import random
@@ -87,7 +159,6 @@ def compute_rewards(data: list):
             ngroup.append(ndatum)
             #! for debugging
             tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
-            print(f"{tokenizer.decode(datum.tokens)=}")
         ndata.append(ngroup)
     return ndata
 
@@ -111,16 +182,28 @@ def compute_advantages(data: list):
         ndata.append(ngroup)
     return ndata
 
-def convert_micro_batch_to_data(micro_batch: list):
+def convert_micro_batch_to_data(micro_batch: list, pad_token_id: int):
     data = []
-    for group in micro_batch:
-        trajs, prompt_ids = group
+    for trajs, prompt_ids in zip(micro_batch.trajs_lst, micro_batch.prompts):
         group_data = []
         for traj in trajs:
             output_ids = traj["output_ids"]
             tokens = torch.tensor(prompt_ids + output_ids)
+            pad_left = micro_batch.max_prompt_len - len(prompt_ids)
+            pad_right = micro_batch.max_response_len - len(output_ids)
+            tokens = torch.nn.functional.pad(
+                tokens,
+                (pad_left, pad_right), # applied to the last dimension
+                "constant",
+                pad_token_id,
+            )
+            assert tokens.shape[0] == micro_batch.max_response_len + micro_batch.max_prompt_len
             # Assumes single-turn conversations
-            response_mask_lst = [0] * len(prompt_ids) + [1] * len(output_ids)
+            response_mask_lst = (
+                [0] * micro_batch.max_prompt_len # prompt masking
+                + [1] * len(output_ids) + [0] * (micro_batch.max_response_len - len(output_ids)) # masking padding tokens
+            )
+            assert len(response_mask_lst) == micro_batch.max_response_len + micro_batch.max_prompt_len
             response_mask =  torch.tensor(response_mask_lst)
             datum = Datum(
                 tokens=tokens,
@@ -144,6 +227,14 @@ async def main(grpo_args: GRPOArgs, inference_server: InferenceServer):
             print(f"waiting...")
             continue
     
+    training_args = TrainingArgs(
+        model_id="Qwen/Qwen3-0.6B",
+        dtype="bfloat16",
+        micro_bsz=4,
+    )
+    trainer = Trainer(training_args)
+    trainer.load_model()
+    
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     test_input = [
         {
@@ -160,15 +251,43 @@ async def main(grpo_args: GRPOArgs, inference_server: InferenceServer):
     batch = input_tokens.expand(4, *input_tokens.shape[1:])
     print(f"{batch.shape=}")
 
-    async for micro_batch in generate_batch(inference_server, batch, grpo_args.group_size, grpo_args.mini_bsz):
+    async for micro_batch in generate_batch(inference_server, batch, grpo_args.group_size, training_args.micro_bsz):
         # print(f"started mock micro_bsz training, {micro_batch}")
-        data = convert_micro_batch_to_data(micro_batch)
+        data = convert_micro_batch_to_data(micro_batch, tokenizer.pad_token_id)
         data = compute_rewards(data)
         data = compute_advantages(data)
-        print(f"{data=}")
         # advantages = compute_advantages()
-        await asyncio.sleep(2)
+        # await asyncio.sleep(2)
+        training_groups = torch.stack([
+            torch.stack([datum.tokens for datum in group])
+            for group in data
+        ])
+        response_masks = torch.stack([
+            torch.stack([datum.response_mask for datum in group])
+            for group in data
+        ])
+        advantage_groups = torch.stack([
+            torch.tensor([datum.advantage for datum in group])
+            for group in data
+        ])
+        #~ Need to make sure it's max_seq_len_compatibe
+        training_micro_batch = training_groups.view((training_args.micro_bsz * grpo_args.group_size, -1))
+        print(f"{training_micro_batch.size()=}")
+        response_masks_micro_batch = response_masks.view((training_args.micro_bsz * grpo_args.group_size, -1))
+        advantage_micro_batch = advantage_groups.view((training_args.micro_bsz * grpo_args.group_size))
+        print(f"{advantage_micro_batch.size()=}")
+
+        mock_logprobs = torch.ones_like(training_micro_batch)
+        mock_old_logprobs = torch.ones(training_micro_batch.shape)
+        trainer.forward_backward(
+            inputs=training_micro_batch,
+            advantages=advantage_micro_batch,
+            old_logprobs=mock_old_logprobs,
+            response_masks=response_masks_micro_batch,
+            loss_function=reinforce_loss,
+        )
         print(f"finished mock micro_bsz training")
+    trainer.optim_step()
 
 
 if __name__ == "__main__":

aghyadrl/train/dev.py
@@ -59,7 +59,7 @@ test_logprobs = trainer.model(test_inputs).logits
 print(f"{test_logprobs=}")
 old_logprobs = torch.gather(test_logprobs, -1, test_inputs.unsqueeze(-1)).squeeze(-1)
 
-trainer.forward_backward(test_inputs, advantages, old_logprobs, vanilla_loss)
+trainer.forward_backward(test_inputs, advantages, old_logprobs, reinforce_loss)
 
 # %%
 import aiohttp
@@ -86,4 +86,59 @@ response = await async_post(
 )
 # %%
 await response.json()
+# %%
+
+from dataclasses import dataclass
+
+@dataclass
+class Test:
+    lst : list
+    val : int
+test = Test([1, 2], 5)
+test.lst.append(3)
+test.val = 9
+print(test)
+# %%
+import torch
+
+max_prompt_len = 6
+max_response_len = 10
+prompt = [1,2,3,4]
+resp = [71, 72, 73, 74, 75, 76]
+tokens = torch.tensor(prompt+resp)
+display(tokens)
+display(torch.nn.functional.pad(
+    tokens,
+    (max_prompt_len - len(prompt), max_response_len - len(resp)),
+    "constant",
+    23432
+))
+
+
+
+# %%
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+t = AutoTokenizer.from_pretrained("Qwen/Qwen3-8b")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-8b")
+
+# %%
+msgs = [
+    {
+        "role": "user",
+        "content": "hello, world!"
+    }
+]
+inp = t.apply_chat_template(msgs, return_tensors="pt")
+inp, "\n", [t.decode(s) for s in inp]
+# %%
+t.pad_token_id, t.eos_token_id, t.pad_token, t.eos_token
+# %%
+out = model(input_ids=inp)
+out
+# %%
+out.logits[0][-1].argmax()
+# %%
+t.decode( out.logits[0][-1].argmax() )
+
 # %%

aghyadrl/train/main.py
@@ -12,7 +12,7 @@ class TrainingArgs:
     dtype: str
 
     # compute args
-    micro_batch_size: int 
+    micro_bsz: int 
 
     # lora args
     lora_rank: int = 32
@@ -29,6 +29,7 @@ class Trainer:
     def __init__(self, training_args):
         self.training_args = training_args
         self.model = None
+        self.optim = None
 
     def load_model(self):
         torch_dtype = self.TORCH_DTYPE_MAP[self.training_args.dtype]
@@ -49,6 +50,7 @@ class Trainer:
         #TODO add option to load an existing lora
         model = get_peft_model(model, lora_config)
         self.model = model
+        self.optim = torch.optim.AdamW(self.model.parameters())
     
     @beartype
     def forward_backward(
@@ -56,13 +58,14 @@ class Trainer:
         inputs: Int[torch.Tensor, "num_inputs max_seq_len"],
         advantages: Float[torch.Tensor, "num_inputs"],
         old_logprobs: Float[torch.Tensor, "num_inputs max_seq_len"],
+        response_masks: Int[torch.Tensor, "num_inputs max_seq_len"],
         loss_function,
     ):
         # inputs: (num_inputs, max_input_len)
         # advantages: (num_inputs)
         #~ Maybe model.train
         #~ do we need padding? 
-        micro_bsz = self.training_args.micro_batch_size
+        micro_bsz = self.training_args.micro_bsz
         #~ this shouldn't stay like this probably cause we maybe have a foreward
         #~ backward on num_inputs not divisible by micro_bsz and it's ok
         assert inputs.shape[0] % micro_bsz == 0, f"forward-backward num_inputs is not divisible by micro batch size. {inputs.shape=}, {micro_bsz=}"
@@ -70,12 +73,19 @@ class Trainer:
         num_iterations = inputs.shape[0] // micro_bsz
         micro_batches: Int[torch.Tensor, "num_iterations micro_bsz max_seq_len"] = inputs.view(num_iterations, micro_bsz, *inputs.shape[1:])
         micro_batches_advantages: Float[torch.Tensor, "num_iterations micro_bsz"] = advantages.view(num_iterations, micro_bsz)
+        micro_batches_masks: Int[torch.Tensor, "num_iterations micro_bsz max_seq_len"] = response_masks.view(num_iterations, micro_bsz, *response_masks.shape[1:])
+        assert micro_batches.shape == micro_batches_masks.shape
         for i in range(num_iterations):
             mb: Int[torch.Tensor, "micro_bsz max_seq_len"] = micro_batches[i]
             mb_advantages: Int[torch.Tensor, "micro_bsz"] = micro_batches_advantages[i]
+            mb_response_masks: Int[torch.Tensor, "micro_bsz max_seq_len"] = micro_batches_masks[i]
             logprobs: Float[torch.Tensor, "micro_bsz max_seq_len vocab_size"] = self.model(mb).logits
-            loss = loss_function(mb, logprobs, old_logprobs, mb_advantages)
+            loss = loss_function(mb, logprobs, old_logprobs, mb_advantages, mb_response_masks)
             loss.backward()
+        
+    def optim_step(self):
+        self.optim.step()
+        self.optim.zero_grad()
 
         
 